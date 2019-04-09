ec166ecdd7762f995f993396d9223c1bd210659e
Otniel <otnieldocs@gmail.com>
2019-04-09 23:02:06 +0700

    add onRefreshListener event listener

--- app/src/main/java/com/paintboard/paintboard/MainActivity.java
+++ app/src/main/java/com/paintboard/paintboard/MainActivity.java
@@ -1,13 +1,43 @@
 package com.paintboard.paintboard;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.util.Log;
+
+import com.paintboard.paintboard.models.ImageContent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    private RecyclerView rvItems;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private List<ImageContent> contents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        swipeRefreshLayout.setOnRefreshListener(this);
+    }
+
+    private void init() {
+        rvItems = findViewById(R.id.rv_items);
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh);
+        contents = new ArrayList<>();
+    }
+
+    @Override
+    public void onRefresh() {
+        /**
+         * Do http request here...
+         */
     }
 }
